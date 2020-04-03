0932fdb4bd50b96b75f03f39b8ad2ca1227b5463
shiyu <2667694325@qq.com>
2020-04-03 18:18:28 +0800

    ç悬浮按钮和可交互提示

--- app/src/main/java/shiyu/learn/materialdesigntrain/MainActivity.java
+++ app/src/main/java/shiyu/learn/materialdesigntrain/MainActivity.java
@@ -1,7 +1,9 @@
 package shiyu.learn.materialdesigntrain;
 
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +12,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -37,6 +40,22 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        FloatingActionButton floatingActionButton=findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this,"FAB clicked",Toast.LENGTH_SHORT).show();
+                Snackbar.make(v,"Data deleted",Snackbar.LENGTH_SHORT)
+                        .setAction("Undo", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(MainActivity.this,"Data restored",Toast.LENGTH_SHORT).show();
+                            }
+                        }).show();
+            }
+        });
+
     }
 
     @Override
