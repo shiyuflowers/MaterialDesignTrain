61b7a85d4a9e0615eb9ac19d92f62a04b74365aa
shiyu <2667694325@qq.com>
2020-04-03 15:46:01 +0800

    DrawerLayout实践

--- app/src/main/java/shiyu/learn/materialdesigntrain/MainActivity.java
+++ app/src/main/java/shiyu/learn/materialdesigntrain/MainActivity.java
@@ -1,13 +1,17 @@
 package shiyu.learn.materialdesigntrain;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +19,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar=findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mDrawerLayout=findViewById(R.id.drawerLayout);
+        ActionBar actionBar=getSupportActionBar();
+        if(actionBar!=null)
+        {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.menu);
+        }
     }
 
     @Override
@@ -27,6 +38,9 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId())
         {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(Gravity.START);
+                break;
             case R.id.item_backup:
                 Toast.makeText(this,"You clicked Backup",Toast.LENGTH_SHORT).show();
                 break;
