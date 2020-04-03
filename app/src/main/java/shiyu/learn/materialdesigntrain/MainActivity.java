a68819ee3e20245f0f80bcd88c78b42af96c850e
shiyu <2667694325@qq.com>
2020-04-03 15:23:43 +0800

    ToolBar Train

--- app/src/main/java/shiyu/learn/materialdesigntrain/MainActivity.java
+++ app/src/main/java/shiyu/learn/materialdesigntrain/MainActivity.java
@@ -2,6 +2,10 @@ package shiyu.learn.materialdesigntrain;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.item_backup:
+                Toast.makeText(this,"You clicked Backup",Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.item_delete:
+                Toast.makeText(this,"You clicked Delete",Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.item_setting:
+                Toast.makeText(this,"You clicked Setting",Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        return true;
     }
 }
