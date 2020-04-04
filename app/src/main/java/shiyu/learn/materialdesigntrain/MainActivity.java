2ce965d630ebd34dc0089bfd01dc0aef9dbfc54f
shiyu <2667694325@qq.com>
2020-04-04 15:51:59 +0800

    实现下拉刷新

--- app/src/main/java/shiyu/learn/materialdesigntrain/MainActivity.java
+++ app/src/main/java/shiyu/learn/materialdesigntrain/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -36,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
     private List<Fruit> mFruitList=new ArrayList<>();
     private FruitAdapter mFruitAdapter;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,6 +84,38 @@ public class MainActivity extends AppCompatActivity {
         mFruitAdapter=new FruitAdapter(mFruitList);
         recyclerView.setAdapter(mFruitAdapter);
 
+        mSwipeRefreshLayout=findViewById(R.id.swipeRefreshLayout);
+        mSwipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary);
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshFruits();
+            }
+        });
+
+    }
+
+    private void refreshFruits() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        initFruits();
+                        mFruitAdapter.notifyDataSetChanged();
+                        mSwipeRefreshLayout.setRefreshing(false);
+                    }
+                });
+
+
+            }
+        }).start();
     }
 
     private void initFruits() {
